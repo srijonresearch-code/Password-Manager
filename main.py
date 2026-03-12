13c1729a3962c9b8b730fb41e3d9f7b302f1d981
Srijon Das <srijon.research@gmail.com>
2026-03-12 22:08:22 +0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Program menu design
+print(f"{"-"*8}Password Manager{"-"*8}")
+print(f"1. Add New Account")
+print(f"2. View All Saved Accounts")
+print(f"3. Update Account Password")
+print(f"4. Search Account By Website")
+print(f"5. Delete Account")
+print(f"6. Generate Secure Password")
+print(f"7. Exit")
+#choose option
+option=int(input("Choose Option: "))
+print(f"{"-"*32}")
